c7d166b20b2307e03e85a06900e49bb14d9ee642
IbnoeRizal <ibnurizal849@gmail.com>
2025-10-17 15:54:49 +0700

    submit form ke payment simulator midtrans

--- app/Http/Controllers/simulateSubmit.php
+++ app/Http/Controllers/simulateSubmit.php
@@ -5,9 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
+use function Laravel\Prompts\form;
+
 class simulateSubmit extends Controller
 {
     public function sendForm(Request $req){
+        $htmlHelper = new \App\Services\Htmlhelper();
+
         // Ambil URL target (endpoint asli simulator Midtrans)
         $url = $req->input('_orig_action');
         if (!$url) {
@@ -20,9 +24,37 @@ class simulateSubmit extends Controller
         // Kirim ke Midtrans pakai POST
         $res = Http::asForm()->post($url, $payload);
 
+        // balikin JSON status gagal, kalau gagal
+        if($res->failed()) return response(["status" => "can't send the url"],$res->status());
 
-        // Balikkan hasil dari Midtrans ke browser (biar bisa dilihat)
-        return response($res->body(), $res->status())
-            ->header('Content-Type', $res->header('Content-Type'));
+        //buat Document object model
+        $dom = $htmlHelper->createDom($res);
+        if(!$dom) return response(["status" => "not a HTML document"],500);
+
+        //copy semua elemen form
+        $forms = $dom->getElementsByTagName("form");
+        if(!$forms) return response(["status" => "no form element"],500);
+
+        $form = $forms->item(0);//pilih indeks 0
+
+        //url endpoint absolut untuk form
+        $absoluteUrl_Endpoint = $htmlHelper->toAbsoluteUrl(
+            $form->getAttribute('action')?: config("midtransAPI.simulator"),
+            config("midtransAPI.simulator")
+        );
+
+        //ganti endpoint ke absolut endpoint
+        $form->setAttribute('action',$absoluteUrl_Endpoint);
+        $htmlHelper->cleanForm($form);
+
+        //jadikan DOM ke array asosiatif, kirim dengan method post
+        $res = Http::asForm()->post($absoluteUrl_Endpoint,$htmlHelper->formToArray($form));
+
+        //gagal kirim ?
+        if($res->failed()) return response(["status" => "can't post to payment simulator"],$res->status());
+
+        //berhasil kirim, tunggu lewat webhook
+        return response(["status" => "checking payment status"],200);
+
     }
 }
