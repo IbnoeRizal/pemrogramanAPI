77f27b92f26414d4cc71b4f76f699a14d5a58808
IbnoeRizal <ibnurizal849@gmail.com>
2025-10-17 15:51:16 +0700

    untuk memisah logika modifikasi DOM untuk php

--- app/Services/Htmlhelper.php
+++ app/Services/Htmlhelper.php
@@ -79,6 +79,8 @@ class Htmlhelper
 
                 // Pindahkan semua input dan button submit dari dalam elemen ini ke parent form
                 foreach ($child->getElementsByTagName('input') as $input) {
+                    //display input as none
+                    $input->setAttribute('style', 'display:none !important;');
                     $node->appendChild($input->cloneNode(true));
                 }
                 foreach ($child->getElementsByTagName('button') as $btn) {
@@ -91,5 +93,33 @@ class Htmlhelper
                 $node->removeChild($child);
             }
         }
+    }
+
+    public function formToArray(\DOMElement $form)
+    {
+        $data = [];
+        foreach ($form->getElementsByTagName('input') as $input) {
+            $name = $input->getAttribute('name');
+            $value = $input->getAttribute('value');
+            if ($name) $data[$name] = $value;
+        }
+        foreach ($form->getElementsByTagName('textarea') as $textarea) {
+            $name = $textarea->getAttribute('name');
+            $value = $textarea->textContent;
+            if ($name) $data[$name] = $value;
+        }
+        foreach ($form->getElementsByTagName('select') as $select) {
+            $name = $select->getAttribute('name');
+            if (!$name) continue;
+            $selected = '';
+            foreach ($select->getElementsByTagName('option') as $option) {
+                if ($option->hasAttribute('selected')) {
+                    $selected = $option->getAttribute('value');
+                    break;
+                }
+            }
+            $data[$name] = $selected;
+        }
+        return $data;
     }
 }
