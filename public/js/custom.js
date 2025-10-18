193ce64e8635cff0578b7467bb02aa866f29cbbc
IbnoeRizal <ibnurizal849@gmail.com>
2025-10-18 22:09:40 +0700

    refactor js, agar reusable

--- public/js/custom.js
+++ public/js/custom.js
@@ -5,23 +5,60 @@ const inputmap = Object.freeze({
     'qris' : 'src'
 });
 
+const statusChecker = function (data,resolve,reject) {
 
-document.addEventListener('DOMContentLoaded',()=>{
-    const paymentType = document.getElementById('tipe-pembayaran').innerHTML.trim();
-    console.log(paymentType);
-    const selector = document.querySelector(map[paymentType]).getAttribute(inputmap[paymentType]);
-    console.log(inputmap[paymentType]);
-    const elem = document.getElementById('confirmation');
+    const fun = (resolve,reject) => setTimeout(()=>{
+        fetch(route.poll,{
+            method : "POST",
+            body: JSON.stringify({ "key": route.orderID}),
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Requested-With': 'XMLHttpRequest',
+                'X-CSRF-TOKEN': data.token
+            },
+            credentials: 'same-origin'
+        })
+        .then(res => res.json())
+        .then( y => {
+            if(y.state === "canceled" || y.state ==="expired")
+                return reject();
+            if(y.state === "settlement")
+                return resolve();
+            else fun(resolve,reject);
+        })
+        .catch(err => reject(err));
+    },3000);
 
-    if(!elem){
-        console.warn(`elemen confirmasi hilang ${elem}`);
-        return;
+    return function (){fun(resolve,reject)};
+
+};
+
+const paySimulation = async function(url ,formData, resolve, reject, {nodeStatus = null} = {}){
+    const flag = nodeStatus instanceof HTMLElement;
+    const res = await fetch(url, {
+        method: "POST",
+        body: formData,
+        headers: {
+        'X-Requested-With': 'XMLHttpRequest', // optional, agar Laravel tahu ini AJAX
+        }
+    });
+
+    const data = await res.json().catch(() => ({}));
+    const next = statusChecker(data,resolve,reject);
+
+    if(flag)nodeStatus.innerText = data.status || `(${res.status})`;
+
+    if (!res.ok) {
+        if(flag)nodeStatus.style.color = "red";
+        throw new Error(`Gagal ${res.status}`);
     }
-    if(!selector){
-        console.warn(`tipe payment salah ${paymentType}`)
-        return;
-    }
 
+    const x = new Promise(next);
+
+    if(flag)nodeStatus.innerText = await x;
+};
+
+const qrisPayment = function(elem,selector){
     elem.innerHTML = "<div> Loading ...</div>";
     fetch(route.form)
     .then(x=>x.text())
@@ -36,66 +73,56 @@ document.addEventListener('DOMContentLoaded',()=>{
         form.addEventListener('submit', async (e) => {
             e.preventDefault(); // cegah submit normal
 
-            // ambil data form sebagai FormData
-            const formData = new FormData(form);
 
             const nwelement = document.createElement('span');
             elem.replaceChildren(nwelement);
             nwelement.innerText = "Wait...";
-            //console.error(`action = ${form.action}\n method = ${form.method.toUpperCase()}`);
+
+            const berhasil = node => node.innerText = "pembayaran berhasil";
+            const gagal = node => node.innerHTML = "pembayaran gagal";
 
 
-            try {
-                const res = await fetch(form.action, {
-                    method: "POST",
-                    body: formData,
-                    headers: {
-                    'X-Requested-With': 'XMLHttpRequest', // optional, agar Laravel tahu ini AJAX
-                    }
-                });
+            paySimulation(
+                form.action,
+                new FormData(form),
+                () => berhasil(nwelement),
+                () => gagal(nwelement),
+                {nodeStatus: nwelement}
+            );
 
-                const data = await res.json().catch(() => ({}));
-
-                nwelement.innerText = data.status || `(${res.status})`;
-
-                if (!res.ok) {
-                    nwelement.style.color = "red";
-                    throw new Error(`Gagal ${res.status}`);
-                }
-
-                const x = new Promise((resolve, reject)=>{
-                    let p = () => setTimeout(()=>{
-
-                        fetch(route.poll,{
-                            method : "POST",
-                            body: JSON.stringify({ "key": route.orderID}),
-                            headers: {
-                                'Content-Type': 'application/json',
-                                'X-Requested-With': 'XMLHttpRequest',
-                                'X-CSRF-TOKEN': data.token
-                            },
-                            credentials: 'same-origin'
-                        })
-                        .then(res => res.json())
-                        .then( y => {
-                            if(y.state === "canceled" || y.state ==="expired")
-                                return reject("pembayaran gagal");
-                            if(y.state === "settlement")
-                                return resolve("pembayaran berhasil");
-                            else p();
-                        })
-                        .catch(err => reject(err));
-                    },3000);
-                    p();
-                });
-
-                nwelement.innerText = await x;
-
-            } catch (err) {
-                console.error('Terjadi error:', err);
-            }
         });
     })
     .catch(err => alert("Gagal", { body: err.message }));
+};
 
-})
+
+//entry point
+document.addEventListener('DOMContentLoaded',()=>{
+    const paymentType = document.getElementById('tipe-pembayaran').innerHTML.trim();
+    console.log(paymentType);
+    const selector = document.querySelector(map[paymentType]).getAttribute(inputmap[paymentType]);
+    console.log(inputmap[paymentType]);
+    const elem = document.getElementById('confirmation');
+
+    if(!elem){
+        console.warn(`elemen konfirmasi hilang ${elem}`);
+        return;
+    }
+    if(!selector){
+        console.warn(`tipe payment salah ${paymentType}`)
+        return;
+    }
+
+    switch (paymentType) {
+        case 'qris':
+            qrisPayment(elem,selector);
+            break;
+
+        default:
+            break;
+    }
+
+
+});
+
+
